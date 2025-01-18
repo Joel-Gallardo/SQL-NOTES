5c2be3f08246d14a6060c97b0ee918ae7219b9e7
Joel Gallardo <joelgallardomn@gmail.com>
2025-01-17 17:06:53 -0700

    ORDER BY Keyword

---
+++ Statements/04_order_by.sql
@@ -0,0 +1,46 @@
+/* The ORDER BY keyword is used to sort the result-set in ascending or descending order.
+
+Example:
+Sort the products by price:
+*/
+SELECT * FROM Products
+ORDER BY Price;
+
+/*
+The ORDER BY keyword sorts the records in ascending order by default. To sort the records in descending order, use the DESC keyword.
+
+Example
+Sort the products from highest to lowest price:
+*/
+SELECT * FROM Products
+ORDER BY Price DESC;
+
+/*
+Order Alphabetically
+For string values the ORDER BY keyword will order alphabetically:
+
+Example
+Sort the products alphabetically by ProductName:
+*/
+SELECT * FROM Products
+ORDER BY ProductName;
+
+/*
+Alphabetically DESC
+To sort the table reverse alphabetically, use the DESC keyword:
+
+Example
+Sort the products by ProductName in reverse order:
+*/
+SELECT * FROM Products
+ORDER BY ProductName DESC;
+
+
+
+-- Table Products
+ProductID	|ProductName	               | SupplierID	|CategoryID	    |Unit	               | Price
+1	        |Chais	                       | 1	        |1	            |10 boxes x 20 bags	   | 18
+2	        |Chang	                       | 1	        |1	            |24 - 12 oz bottles	   | 19
+3	        |Aniseed Syrup	               | 1	        |2	            |12 - 550 ml bottles   | 10
+4	        |Chef Anton's Cajun Seasoning  | 2	        |2	            |48 - 6 oz jars	       | 22
+5	        |Chef Anton's Gumbo Mix	       | 2	        |2	            |36 boxes	           | 21.35
